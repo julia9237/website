d9f39f5947ac1b85d6375c32b7494c45ba14e170
nlbence <40839895+nlbence@users.noreply.github.com>
2020-05-06 15:50:45 +0200

    Added icon switcher

--- src/js/cs.js
+++ src/js/cs.js
@@ -1,4 +1,14 @@
-function colorSwitch() {
+function stuffSwitch() {
+    var iconWB = "src/img/icon-wb.png"
+    var iconBW = "src/img/icon-bw.png"
     var element = document.getElementById("switch");
     element.classList.toggle("inverted");
+    
+    var iconElem = document.getElementsByClassName("icon")[0];
+    var iconCurrent = iconElem.getAttribute("href");
+    console.log(iconCurrent)
+    if(iconCurrent == iconWB) {
+        iconElem.setAttribute("href", iconBW);
+    } else { iconElem.setAttribute("href", iconWB) }
+
 }
