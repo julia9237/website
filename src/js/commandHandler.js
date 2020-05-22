47500052f504255180edacc9b6fe46c205f35a3a
nlbence <bence.sw@gmail.com>
2020-05-22 21:04:22 +0200

    Command checker

--- src/js/commandHandler.js
+++ src/js/commandHandler.js
@@ -1,18 +1,34 @@
+var commands = ["help"];
+
 function submitt() {
     var lastCommand = document.getElementById("focus");
-    if(lastCommand && lastCommand.value)
-    {
-    lastCommand.setAttribute("readonly", "");
-    lastCommand.setAttribute("id", "");
-    lastCommand.removeAttribute("focus");
+    if (lastCommand && lastCommand.value) {
+        lastCommand.setAttribute("readonly", "");
+        lastCommand.setAttribute("id", "");
+        lastCommand.removeAttribute("focus");
 
-    var newCommand = document.createElement("input");
-    document.getElementById("main").appendChild(newCommand)
-    newCommand.setAttribute("class", "cmd")
-    newCommand.setAttribute("id", "focus")
-    newCommand.setAttribute("autocomplete", "off")
-    newCommand.setAttribute("spellcheck", "false")
-    newCommand.setAttribute("maxlength", "56")
+        var commandResponse = document.createElement("p");
+        document.getElementById("main").appendChild(commandResponse);
+        if(commands.includes(lastCommand.value)) {
+            switch(lastCommand.value) {
+                case "help":
+                    commandResponse.innerHTML = lastCommand.value + ": help text";
+                    break;
+            
+                default:
+                    break;
+            }
+        } else {
+            commandResponse.innerHTML = lastCommand.value + ': command not found';
+        }
+
+        var newCommand = document.createElement("input");
+        document.getElementById("main").appendChild(newCommand);
+        newCommand.setAttribute("class", "cmd")
+        newCommand.setAttribute("id", "focus")
+        newCommand.setAttribute("autocomplete", "off")
+        newCommand.setAttribute("spellcheck", "false")
+        newCommand.setAttribute("maxlength", "56")
     };
 };
 
