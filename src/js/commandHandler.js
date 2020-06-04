a8e9af65ac13a820d870aa2c73e1fc8e5d96c964
nlbence <bence.sw@gmail.com>
2020-06-04 21:32:28 +0200

    Electron and exit commands

--- src/js/commandHandler.js
+++ src/js/commandHandler.js
@@ -1,6 +1,25 @@
-var validCommands = ["help", "theme", "themes"];
+var exitCommands = ["close", "quit", "exit", "stop"]
+var validCommands = [
+    "help",
+    "theme",
+    "themes"
+] + exitCommands;
+
 var hiddenCommands = ["dicks"]
 var themes = ["dark", "light", "purple"];
+
+function focusCMD() {
+    document.getElementById("focus").focus();
+};
+
+function resizeTextarea() {
+    var lastCommand = document.getElementById("focus");
+    if (lastCommand.value.length != 0) {
+        var divGetHeight = document.getElementById("getHeight");
+        divGetHeight.innerHTML = lastCommand.value;
+        lastCommand.setAttribute("style", "height: " + divGetHeight.offsetHeight + "px");
+    };
+};
 
 function themeSwitch(themeName) {
     document.getElementById("switch").setAttribute("class", themeName);
@@ -31,7 +50,7 @@ function submitt() {
                     break;
                 case "help":
                     commandResponse.innerHTML = commandParts[0] + ": " + validCommands;
-                    break;
+					break;
                 case "theme":
                     if (commandParts.length > 1) {
                         if (themes.includes(commandParts[1])) {
@@ -44,10 +63,18 @@ function submitt() {
                         commandResponse.innerHTML = "Themes: " + themes;
                     };
                     break;
-
                 case "themes":
                     commandResponse.innerHTML = "Themes: " + themes;
                     break;
+                case "stop":
+                case "close":
+                case "quit":
+                case "exit":    
+                    window.close();
+                    document.getElementsByTagName("body")[0].setAttribute("style", "display: none;");
+                    document.getElementById("switch").setAttribute("style", "background-color: #000;");
+                    break;
+
             };
         } else {
             commandResponse.innerHTML = commandParts[0] + ': command not found';
@@ -58,22 +85,16 @@ function submitt() {
     document.getElementById("main").appendChild(newCommand);
     newCommand.setAttribute("class", "cmd");
     newCommand.setAttribute("id", "focus");
+    focusCMD();
     newCommand.setAttribute("autocomplete", "off");
     newCommand.setAttribute("spellcheck", "false");
     lastCommand.removeAttribute("id");
-};
-
-function resizeTextarea() {
-    var lastCommand = document.getElementById("focus");
-    if (lastCommand.value.length != 0) {
-        var divGetHeight = document.getElementById("getHeight");
-        divGetHeight.innerHTML = lastCommand.value;
-        lastCommand.setAttribute("style", "height: " + divGetHeight.offsetHeight + "px");
-    };
+    resizeTextarea();
 };
 
 document.addEventListener('keydown', function (event) {
     resizeTextarea();
+    focusCMD();
     if (event.keyCode == 13) {
         submitt();
     };
@@ -81,14 +102,12 @@ document.addEventListener('keydown', function (event) {
 
 document.addEventListener('keypress', function (event) {
     resizeTextarea();
+    focusCMD();
 });
 
 document.addEventListener('keyup', function (event) {
     resizeTextarea();
+    focusCMD();
 });
-
-function focusCMD() {
-    document.getElementById("focus").focus();
-};
 
 window.onload = focusCMD();
