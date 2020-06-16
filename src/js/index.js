7efa7438d61244288999bfcf9a6ef7ef01211f56
nlbence <bence.sw@gmail.com>
2020-06-16 07:46:56 +0200

    Colored icons

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 function getRandomBackground() {
-    return "background-color: var(--background" + (Math.floor(Math.random() * 9) + 1) + ");";
+    var bgNum = Math.floor(Math.random() * 9) + 1;
+    document.getElementsByClassName("icon")[0].setAttribute("href", "./src/img/icon" + bgNum + ".png");
+    return "background-color: var(--background" + bgNum + ");";
 }
 ;
 var html = document.getElementsByTagName("html")[0];
