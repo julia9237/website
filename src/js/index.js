0a2114d9a9618d7c83b237de6b0771b72d91d1a3
nlbence <bence.sw@gmail.com>
2020-06-16 08:35:23 +0200

    Custom hex

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,20 @@ function getRandomBackground() {
 ;
 var html = document.getElementsByTagName("html")[0];
 html.setAttribute("style", getRandomBackground());
+var urlParams = new URLSearchParams(window.location.search); // get queries
+if (urlParams.has("hex")) { // handle queries
+    var hex = urlParams.get("hex");
+    document.getElementsByTagName("html")[0].setAttribute("style", "background-color: #" + hex + ";");
+    document.getElementsByClassName("icon")[0].setAttribute("href", "");
+    /* find og:image meta and set it to none
+    var metas = document.getElementsByTagName("meta");
+    for(var i = 0; i < metas.length; i++) {
+      switch(metas[i].getAttribute("property")) {
+        case "og:image":
+          metas[i].setAttribute("content", "");
+          break;
+      };
+    };
+    */
+}
+;
