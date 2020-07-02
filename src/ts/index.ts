0364275835322b94a2c9d4155765abbff411334b
Aluuuuuu <bence.sw@gmail.com>
2020-07-02 15:23:07 +0200

    Fix About

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,17 @@
 document.getElementsByTagName("h1")[0].onclick = function() {window.location.href = "./hi.html"}
 
+document.getElementById("about").onclick = function() {
+  const about = document.getElementById("abouttext")
+
+  if(about.getAttribute("style") === "") {
+    about.setAttribute("style", "display: none;")
+  }
+
+  else {
+    about.setAttribute("style", "")
+  }
+}
+
 /*function getRandomBackground(): String {
 
   const colors = [
