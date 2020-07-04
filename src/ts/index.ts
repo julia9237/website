fd4b84f8631329be9bc5565544f8f87d6f920210
Aluuuuuu <bence.sw@gmail.com>
2020-07-05 00:47:29 +0200

    Swap order in onclick

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,8 @@
 document.getElementsByTagName("h1")[0].onclick = function() {window.location.href = "./hi.html"}
 
 document.getElementById("about").onclick = function() {
-  const about = document.getElementById("abouttext")
   window.getSelection().removeAllRanges()
+  const about = document.getElementById("abouttext")
 
   if(about.getAttribute("style") === "") {
     about.setAttribute("style", "display: none;")
