3ea5d9abb5a186643ceda5fe8a48d4366a0f1785
BuildTools <unconfigured@null.spigotmc.org>
2020-11-09 05:16:15 +0100

    start redo

--- src/js/index-faviconSwitcher.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var _a;
-const favNum = Math.floor(Math.random() * 4) + 1;
-(_a = document.getElementById("favicon")) === null || _a === void 0 ? void 0 : _a.setAttribute("href", `src/img/favicon/yooooo${favNum}.png`);
